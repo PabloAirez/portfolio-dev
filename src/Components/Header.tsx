3011aa4edc7dc084caf6367a9e10d3bf75db08e2
PabloAirez <pabloadscontato@gmail.com>
2025-02-11 14:34:52 -0300

    Inicia o projeto do novo portfolio, criado em React com Typescript

---
+++ src/Components/Header.tsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+    return (
+        <header className='w-full h-96 bg-red-500 bg-center bg-cover bg-[url("/src/assets/img/background.gif")]'>  
+            <h1 className='text-3xl font-bold text-center text-white '>Pablo Santos</h1>  
+            <h2 className='text-xl font-bold text-center text-white'>Desenvolvedor Fullstack</h2>
+        </header>
+    );
+};
+
+export default Header;
