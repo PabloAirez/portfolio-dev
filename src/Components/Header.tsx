4861eae978f5035dd6c8e0c4484fdc77ab836f3b
PabloAirez <pabloadscontato@gmail.com>
2025-02-17 15:09:23 -0300

    removendo bg-red do header deixado por engano

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
     return (
-        <header className='w-full h-96 bg-red-500 bg-center bg-cover bg-black/60'>  
+        <header className='w-full h-96 bg-center bg-cover bg-black/60'>  
             <div className='flex flex-col items-center gap-6 justify-center h-full'>
                 <h1 className='text-5xl font-bold text-center text-white hover:text-purple-500 duration-300 animate-fade-in'>Pablo Santos</h1>  
                 <h2 className='text-2xl font-bold text-center text-white hover:text-purple-500 duration-300 animate-fade-in'>Desenvolvedor Fullstack</h2>
