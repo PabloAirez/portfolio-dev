5fd30f70f78c9ec3403d13cafb214427e3211d06
PabloAirez <pabloadscontato@gmail.com>
2025-02-17 15:22:55 -0300

    adiciona link de contato que direciona para o linkedin

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
                 <h2 className='text-2xl font-bold text-center text-white hover:text-purple-500 duration-300 animate-fade-in'>Desenvolvedor Fullstack</h2>
                 <div className='flex gap-4'>
                     <button className='bg-purple-500 text-white font-bold py-2 px-4 rounded hover:bg-purple-700 duration-300 animate-fade-in'><a href="/cv.pdf" download>Download CV</a></button>
-                    <button className='bg-purple-500 text-white font-bold py-2 px-4 rounded hover:bg-purple-700 duration-300 animate-fade-in'>Contato</button>
+                    <button className='bg-purple-500 text-white font-bold py-2 px-4 rounded hover:bg-purple-700 duration-300 animate-fade-in'><a href="https://www.linkedin.com/messaging/compose/?recipient=pablo-santos-712311223" target="_blank">Contato</a></button>
                 </div>
             </div>
 
